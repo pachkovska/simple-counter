5c26616ea4a0bfc55e42d35b5aa77aa8c63ad4c5
pachkovska <pachkovska@gmail.com>
2020-04-02 20:20:40 -0700

    css additions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import _ from 'lodash';
 import './App.css';
 import IncrementButton from "./components/IncrementButton";
@@ -24,37 +24,39 @@ function App() {
 
     return (
         <div className="App">
-            <div className="App-header">Please enter numbers from 1 to 20</div>
-            <ValueInput
-                title="Begin incrementing at: "
-                onChange={handleValueChange}
-                name="fromValue"
-                className="valueInput-from"
-            />
-            <ValueInput
-                title="End incrementing at: "
-                onChange={handleValueChange}
-                name="toValue"
-                className="valueInput-to"
-            />
-            <div className="number">
-                {number}
-            </div>
-            <div className="incrementButtons">
-                {
-                    values.fromValue && values.toValue &&
-                    _.range(values.fromValue, values.toValue + 1, 1).map(el => (
-                        <IncrementButton key={el} step={el} incrementNumber={() => incrementNumber(el)}/>
-                    ))
-                }
-            </div>
-            <div className="decrementButtons">
-                {
-                    values.fromValue && values.toValue &&
-                    _.range(values.fromValue, values.toValue + 1, 1).map(el => (
-                        <IncrementButton key={el} step={-el} incrementNumber={() => incrementNumber(-el)}/>
-                    ))
-                }
+            <div className="App-content">
+                <div className="App-header">Please enter numbers from 1 to 20</div>
+                <ValueInput
+                    title="Begin incrementing at: "
+                    onChange={handleValueChange}
+                    name="fromValue"
+                    className="valueInput-from"
+                />
+                <ValueInput
+                    title="End incrementing at: "
+                    onChange={handleValueChange}
+                    name="toValue"
+                    className="valueInput-to"
+                />
+                <div className="number">
+                    {number}
+                </div>
+                <div className="incrementButtons">
+                    {
+                        values.fromValue && values.toValue &&
+                        _.range(values.fromValue, values.toValue + 1, 1).map(el => (
+                            <IncrementButton key={el} step={el} incrementNumber={() => incrementNumber(el)}/>
+                        ))
+                    }
+                </div>
+                <div className="decrementButtons">
+                    {
+                        values.fromValue && values.toValue &&
+                        _.range(values.fromValue, values.toValue + 1, 1).map(el => (
+                            <IncrementButton key={el} step={-el} incrementNumber={() => incrementNumber(-el)}/>
+                        ))
+                    }
+                </div>
             </div>
         </div>
     );
@@ -62,12 +64,21 @@ function App() {
 
 export default App;
 
-{/* Original version of input fields. In the current version Input is a separate component*/}
-{/*<div className="valueInput--from">*/}
-{/*    <div className="valueInput-title">Begin Incrementing at:</div>*/}
-{/*    <input type="text" name="fromValue" onChange={handleValueChange}></input>*/}
-{/*</div>*/}
-{/*<div className="valueInput--to">*/}
-{/*    <div className="valueInput-title">End Incrementing at:</div>*/}
-{/*    <input type="text" name="toValue" onChange={handleValueChange}></input>*/}
-{/*</div>*/}
+{/* Original version of input fields. In the current version Input is a separate component*/
+}
+{/*<div className="valueInput--from">*/
+}
+{/*    <div className="valueInput-title">Begin Incrementing at:</div>*/
+}
+{/*    <input type="text" name="fromValue" onChange={handleValueChange}></input>*/
+}
+{/*</div>*/
+}
+{/*<div className="valueInput--to">*/
+}
+{/*    <div className="valueInput-title">End Incrementing at:</div>*/
+}
+{/*    <input type="text" name="toValue" onChange={handleValueChange}></input>*/
+}
+{/*</div>*/
+}
