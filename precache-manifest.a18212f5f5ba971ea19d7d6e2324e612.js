f1c9bb4d9e42ff28dafc3b5302816f4504e994a3
pachkovska <pachkovska@gmail.com>
2020-04-02 20:29:54 -0700

    Updates

---
+++ precache-manifest.a18212f5f5ba971ea19d7d6e2324e612.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04402503fe85d18cbfd850815cc76cc1",
+    "url": "/simple-counter/index.html"
+  },
+  {
+    "revision": "add96a95a0bf83a250c5",
+    "url": "/simple-counter/static/css/main.83b92b23.chunk.css"
+  },
+  {
+    "revision": "945b4c6cc0b56e0549ee",
+    "url": "/simple-counter/static/js/2.20ea216c.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/simple-counter/static/js/2.20ea216c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "add96a95a0bf83a250c5",
+    "url": "/simple-counter/static/js/main.6bcae0e2.chunk.js"
+  },
+  {
+    "revision": "d9df88c62bf0b4852f90",
+    "url": "/simple-counter/static/js/runtime-main.d65f9511.js"
+  }
+]);
